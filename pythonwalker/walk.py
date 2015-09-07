c92cbdb40d74d8bc585bd27cd0701386a0861b8e
Tim Bradshaw <tim.bradshaw@metoffice.gov.uk>
2015-09-07 12:24:24 +0100

    User-provided identity function.
    This allows the user to determine how the hash value of an object
    should be computed, rather than relying on id.  So for instance this
    will help in the case where objects are strings: you can just provide
    an argument of lambda o: o.__hash__().
    
    Signed-off-by: Tim Bradshaw <tfb@cley.com>

--- pythonwalker/walk.py
+++ pythonwalker/walk.py
@@ -19,7 +19,7 @@ class TooDeep(Limitation):
 
 def walk_object(root, visitor=lambda o, d, p, n: d,
                 fabricator=lambda o: None, combiner=lambda d1, d2: None,
-                walkers=None, seen=None, maxdepth=100,
+                identity=id, walkers=None, seen=None, maxdepth=100,
                 root_parent=None, root_name=None):
     # Walk an object and its children.
     #
@@ -38,10 +38,37 @@ def walk_object(root, visitor=lambda o, d, p, n: d,
     # For the root object the root_parent and root_name variables are
     # used as the parent and name-in-parent arguments.
     #
-    # The seen argument is a set containing the id()s of object which
-    # should be considered to have already been seen, or None.  The
-    # ids of objects are used because very many objects are not
-    # hashable, although their ids are (hello, Earth to Python?).
+    # An occurs check is needed to prevent loops (if a->b->a, then the
+    # function must not loop, and the visit order is for a walk
+    # starting at a is: a with b as parent, b with a as parent and a
+    # with no parent (starting from b it would be b with a as parent,
+    # a with b as parent, b with no parent).  To do this check the
+    # function must keep track of what it has seen already, which it
+    # does using a set.  Not all objects are hashable in Python, and
+    # in any case whether two objects represent the same thing is
+    # application-dependent.  This is supported with two arguments:
+    # seen and identity.
+    #
+    # The identity argument is a function which should return a
+    # hashable value from each object, in such a way that two objects
+    # which are the same for the purposes of the program have the same
+    # value.  The default value of this is the id function, which
+    # returns a unique, hashable value for any Python object.
+    #
+    # The seen argument is a set containing the values returned by the
+    # identity function of objects which should be considered to have
+    # already been seen.  If no value is given (or it is given as
+    # None) then a set will be constructed.  The function mutates this set.
+    #
+    # You only need to provide the seen argument if you want to make
+    # several walks (see walk_modules for an example): normally it's
+    # fine to let the function fabricate one.  The notion of identity
+    # is complex: if you use the default (id) then, for instance, not
+    # all numbers with the same value and type will be considered
+    # identical, and strings with the same content will also often not
+    # be identical.  Python unfortunately has not thought to provide
+    # many useful tools here (no real equivalent of EQL or EQL
+    # hashtables, for instance).
     #
     # maxdepth says how deep to go before raising a TooDeep exception.
     #
@@ -50,7 +77,8 @@ def walk_object(root, visitor=lambda o, d, p, n: d,
     # will work to exercise the walkers.
     #
     # Note that this is a post-order walk: the object is visited after
-    # its children.
+    # its children, so that it can combine the values from its
+    # children.
     #
     # Note that objects which have already been visited are visited
     # again if they appear as children of other nodes, but their
@@ -68,7 +96,7 @@ def walk_object(root, visitor=lambda o, d, p, n: d,
         if depth >= maxdepth:
             raise TooDeep("too deep", depth)
 
-        hashable = id(it)
+        hashable = identity(it)
         data = None
         seenp = False
         walked_children_p = False
@@ -95,7 +123,7 @@ def walk_object(root, visitor=lambda o, d, p, n: d,
 def walk_modules(modules=sys.modules,
                  visitor=lambda o, d, p, n: d,
                  fabricator=lambda o: None, combiner=lambda d1, d2: None,
-                 walkers=None, seen=None, maxdepth=100,
+                 identity=id, walkers=None, seen=None, maxdepth=100,
                  root_parent=None, root_name=None, for_side_effect=False):
 
     seen = set() if not seen else seen
@@ -103,8 +131,8 @@ def walk_modules(modules=sys.modules,
     def wlk(name, mod):
         return walk_object(mod, visitor=visitor,
                            fabricator=fabricator, combiner=combiner,
-                           walkers=walkers, seen=seen, maxdepth=maxdepth,
-                           root_parent=None, root_name=name)
+                           identity=identity, walkers=walkers, seen=seen,
+                           maxdepth=maxdepth, root_parent=None, root_name=name)
 
     if for_side_effect:
         for (name, mod) in modules.iteritems():
